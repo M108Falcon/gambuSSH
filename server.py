43c52569c545d555b0f584f6c71fa909dfe46827
M108Falcon <mridulwadhwa2000@gmail.com>
2021-02-27 18:54:41 +0530

    Basic server setup

---
+++ server.py
@@ -0,0 +1,54 @@
+import socket
+import threading
+
+# set Port number
+PORT = 8000
+
+# get local machine address
+SERVER = socket.gethostbyname(socket.gethostname())
+
+# store info in tuple for network socket
+ADDR = (SERVER, PORT)
+
+# format
+FORMAT = 'utf-8'
+
+# Connection Header of 64bytes
+HEADER = 64
+
+# disconnection message
+DISCONNECT_MESSAGE = "!disconnect"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+
+def handleClient(conn, address):
+    print(f"[NEW CONNECTION] {address} connected.")
+
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+
+            print(f"[{address}] {msg}")
+            conn.send("Msg received".encode(FORMAT))
+    conn.close()
+
+
+def startConneciton():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, address = server.accept()
+        thread = threading.Thread(target=handleClient, args=(conn, address))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+
+print(f"[STARTING] Server is starting on PORT {PORT}")
+startConneciton()
